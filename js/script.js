6b43b0ae76f76cb152a8e90df3a2df97b22bbf11
Ryan Hans <primaryryanhans@gmail.com>
2025-08-14 12:27:49 +0530

    Added Backdrop Image to Movie-Details Page

--- js/script.js
+++ js/script.js
@@ -81,7 +81,12 @@ async function displayPopularShows() {
 // Display movie details
 async function displayMovieDetails() {
   const movieID = window.location.search.split("=")[1];
+
   const movie = await fetchAPIData(`movie/${movieID}`);
+
+  // Overlay for background image
+  displayBackgroundImage("movie", movie.backdrop_path);
+
   const div = document.createElement("div");
   div.innerHTML = `<div class="details-top">
           ${
@@ -138,6 +143,28 @@ async function displayMovieDetails() {
             .join(", ")}</div>
         </div>`;
   document.querySelector("#movie-details").appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url("https://images.tmdb.org/t/p/original/${backgroundPath}")`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
 
 // Fetch data from TMDB API
