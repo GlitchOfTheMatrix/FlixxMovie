41e898229c0a9af9ef38282ee4ecafd25eac36a5
Ryan Hans <primaryryanhans@gmail.com>
2025-08-14 23:28:16 +0530

    Display Search Results Added

--- js/script.js
+++ js/script.js
@@ -250,11 +250,59 @@ async function search() {
 
   if (global.search.term !== "" && global.search.term !== null) {
     // @todo - make request and display results
-    const results = await searchAPIData();
-    console.log(results);
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert("No Results Found");
+    }
+
+    displaySearchResults(results);
+
+    // Clear Input
+    document.querySelector("#search-term").value = "";
   } else {
     showAlert("Please enter a search term");
   }
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+            <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://images.tmdb.org/t/p/w500${result.poster_path}"
+              class="card-img-top"
+              alt="${
+                global.search.type === "movie" ? result.title : result.name
+              }"
+              />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${
+                global.search.type === "movie" ? result.title : result.name
+              }"
+            />`
+            }
+            </a>
+              <div class="card-body">
+              <h5 class="card-title">${
+                global.search.type === "movie" ? result.title : result.name
+              }</h5>
+              <p class="card-text">
+                <small class="text-muted">Release: ${
+                  global.search.type === "movie"
+                    ? result.release_date
+                    : result.first_air_date
+                }</small>
+              </p>
+            </div>`;
+    document.querySelector("#search-results").appendChild(div);
+  });
 }
 
 async function displaySlider() {
@@ -327,8 +375,7 @@ async function fetchAPIData(endpoint) {
 
 // Search
 async function searchAPIData() {
-  const API_KEY = global.api.apiKey; // If it were production site, this api key would be at the backend, so that nobody could get the api key.
-
+  const API_KEY = global.api.apiKey;
   const API_URL = global.api.apiUrl;
 
   showSpinner();
@@ -364,7 +411,7 @@ function highlightActiveLink() {
 }
 
 // Show Alert
-function showAlert(message, className) {
+function showAlert(message, className = "alert-error") {
   const alertEl = document.createElement("div");
   alertEl.classList.add("alert", className);
   alertEl.appendChild(document.createTextNode(message));
