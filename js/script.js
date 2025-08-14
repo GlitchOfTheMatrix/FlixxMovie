ccd37a2f0210ec91f9fc556ffa2efab75b55c561
Ryan Hans <primaryryanhans@gmail.com>
2025-08-14 23:05:20 +0530

    Added search function and custom alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey: "7558d1f258135c1b5e1ad8bf33dca8a1",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
 
 // Display 20 most Popular movies
@@ -230,6 +240,23 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    // @todo - make request and display results
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert("Please enter a search term");
+  }
+}
+
 async function displaySlider() {
   const { results } = await fetchAPIData("movie/now_playing");
 
@@ -282,9 +309,9 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = "7558d1f258135c1b5e1ad8bf33dca8a1"; // If it were production site, this api key would be at the backend, so that nobody could get the api key.
+  const API_KEY = global.api.apiKey; // If it were production site, this api key would be at the backend, so that nobody could get the api key.
 
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
@@ -294,10 +321,23 @@ async function fetchAPIData(endpoint) {
 
   const data = await response.json();
 
-  // For actually viewing the spinner, because it is so fast!!
-  // setTimeout(() => {
-  //   hideSpinner();
-  // }, 500);
+  hideSpinner();
+  return data;
+}
+
+// Search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey; // If it were production site, this api key would be at the backend, so that nobody could get the api key.
+
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+
+  const data = await response.json();
 
   hideSpinner();
   return data;
@@ -323,6 +363,16 @@ function highlightActiveLink() {
   });
 }
 
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 function addCommasToNumberRegex(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -345,7 +395,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
   highlightActiveLink();
